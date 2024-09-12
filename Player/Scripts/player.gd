07966efda6d9ab78bae3eda4d9bfcb69d6e956f9
Juan Manuel Gomez <juan@lucci.ai>
2024-09-12 16:03:57 -0700

    Added basic state machine idle and walk

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -3,14 +3,14 @@ extends CharacterBody2D
 
 var cardinal_direction: Vector2 = Vector2.DOWN
 var direction: Vector2 = Vector2.ZERO
-var move_speed: float = 100.0
-var state: String = "idle"
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
 
 
 func _ready():
+	state_machine.Initialize(self)
 	pass
 
 func _process(delta):
@@ -18,10 +18,6 @@ func _process(delta):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left") 
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up") 
 	
-	velocity = direction * move_speed
-	
-	if SetState() == true || SetDirection() == true:
-		UpdateAnimation()
 
 func _physics_process(delta):
 	move_and_slide()
@@ -43,14 +39,7 @@ func SetDirection() -> bool:
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 
-func SetState() -> bool:
-	var new_state: String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-
-func UpdateAnimation() -> void:
+func UpdateAnimation(state: String) -> void:
 	animation_player.play(state + "_" + AnimDirection())
 
 func AnimDirection() -> String:
---
+++ Player/Scripts/state_idle.gd
@@ -0,0 +1,22 @@
+class_name State_Idle
+extends State
+
+@onready var walk: State = $"../Walk"
+
+func Enter() -> void:
+	player.UpdateAnimation("idle")
+
+func Exit() -> void:
+	pass
+
+func Process(delta: float) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+
+func Physics(delta: float) -> State:
+	return null
+
+func HandleInput(_event: InputEvent) -> State:
+	return null
